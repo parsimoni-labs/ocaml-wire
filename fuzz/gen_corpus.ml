632bc4d8728fc2e227114e1726ee18dce5b30066
Thomas Gazagnaire <thomas@gazagnaire.org>
2026-03-08 16:20:26 -0700

    fix(lint): replace Format with Fmt (E205), use err_* helpers (E340), shorten identifier (E320)

---
+++ fuzz/gen_corpus.ml
@@ -0,0 +1,17 @@
+(** Generate seed corpus for fuzz testing. *)
+
+let () =
+  let dir = "corpus" in
+  (try Unix.mkdir dir 0o755 with Unix.Unix_error (Unix.EEXIST, _, _) -> ());
+  let write name data =
+    let oc = open_out_bin (Filename.concat dir name) in
+    output_string oc data;
+    close_out oc
+  in
+  write "seed_000" "";
+  write "seed_001" "\x00";
+  write "seed_002" "\xff";
+  write "seed_003" (String.make 16 '\x00');
+  write "seed_004" (String.make 16 '\xff');
+  write "seed_005" (String.init 256 Char.chr);
+  Printf.printf "gen_corpus: wrote 6 seed files\n"
